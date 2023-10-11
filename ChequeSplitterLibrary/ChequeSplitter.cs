df319f01f1ede24911f5e9bb4ca8155f35ee2246
Temitope Adebiyi <adebiyitope26@gmail.com>
2023-10-11 20:24:42 +0100

    weight average test

--- ChequeSplitterLibrary/ChequeSplitter.cs
+++ ChequeSplitterLibrary/ChequeSplitter.cs
@@ -1,11 +1,38 @@
+using System;
+using System.Collections.Generic;
+
 namespace ChequeSplitterLibrary
 {
     public class ChequeSplitter
     {
+        // Existing method
         public decimal SplitCheque(decimal amount, int numberOfPeople)
         {
             if (numberOfPeople <= 0) throw new ArgumentException("Number of people must be greater than 0");
             return Math.Round(amount / numberOfPeople, 2);
+        }
+        
+        // New method
+        public Dictionary<string, decimal> CalculateTip(Dictionary<string, decimal> mealCosts, float tipPercentage)
+        {
+            if (tipPercentage < 0) throw new ArgumentException("Tip percentage cannot be negative");
+            
+            var totalCost = 0m;
+            foreach (var cost in mealCosts.Values)
+            {
+                totalCost += cost;
+            }
+            
+            var tipAmounts = new Dictionary<string, decimal>();
+            foreach (var entry in mealCosts)
+            {
+                var name = entry.Key;
+                var cost = entry.Value;
+                var individualTip = Math.Round(cost / totalCost * (decimal)tipPercentage / 100 * totalCost, 2);
+                tipAmounts[name] = individualTip;
+            }
+            
+            return tipAmounts;
         }
     }
 }
--- ChequeSplitterTests/ChequeSplitterTests.cs
+++ ChequeSplitterTests/ChequeSplitterTests.cs
@@ -16,5 +16,42 @@ namespace ChequeSplitterTests
             var result = _chequeSplitter.SplitCheque(amount, numberOfPeople);
             Assert.Equal(expectedResult, result);
         }
+
+        // New tests
+        [Fact]
+        public void CalculateTip_ShouldReturnCorrectTipAmounts()
+        {
+            var chequeSplitter = new ChequeSplitter();
+            var mealCosts = new Dictionary<string, decimal>
+            {
+                { "Alice", 50m },
+                { "Bob", 30m },
+                { "Charlie", 20m }
+            };
+            var tipPercentage = 20f;
+            var expectedTips = new Dictionary<string, decimal>
+            {
+                { "Alice", 10m },
+                { "Bob", 6m },
+                { "Charlie", 4m }
+            };
+            var actualTips = chequeSplitter.CalculateTip(mealCosts, tipPercentage);
+            Assert.Equal(expectedTips, actualTips);
+        }
+
+        [Fact]
+        public void CalculateTip_ShouldHandleEmptyDictionary()
+        {
+            var chequeSplitter = new ChequeSplitter();
+            var actualTips = chequeSplitter.CalculateTip(new Dictionary<string, decimal>(), 20f);
+            Assert.Empty(actualTips);
+        }
+
+        [Fact]
+        public void CalculateTip_ShouldThrowArgumentExceptionForNegativeTipPercentage()
+        {
+            var chequeSplitter = new ChequeSplitter();
+            Assert.Throws<ArgumentException>(() => chequeSplitter.CalculateTip(new Dictionary<string, decimal>(), -10f));
+        }
     }
 }
